d328b44981eb398866aaa29fcaaf66d6e1189f4f
margothjenny6-a11y <margothjenny6@gmail.com>
2026-04-21 19:20:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 import os
-import asyncio
-import time
 import threading
-from telegram import Bot
-from config import TOKEN
 from server import app
 from bot import correr_bot
-
-async def limpiar_sesion():
-    bot = Bot(token=TOKEN)
-    await bot.delete_webhook(drop_pending_updates=True)
-    print("[OK] Sesion de Telegram limpiada")
-
-asyncio.run(limpiar_sesion())
-asyncio.set_event_loop(asyncio.new_event_loop())   # ← agregar esta línea
-time.sleep(3)
-
 
 port = int(os.environ.get("PORT", 5000))
 
@@ -28,3 +14,4 @@ hilo_servidor.start()
 print(f"[OK] Servidor corriendo en puerto {port}")
 print("[OK] Bot de Telegram activo, esperando solicitudes...")
 correr_bot()
+
